60979ef428759f14a61c2800c9dba627092e6afd
Santhosh Ganesan <santhoshganesan2004@gmail.com>
2025-06-20 13:21:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/", // Updated for root-level deployment
-}));
+  base: "/", // Correct for root-level deployment
+});
